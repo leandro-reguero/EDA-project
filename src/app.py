fd9e3b2a24bee971ff65c2d8531f53689301996c
leandro-reguero <lreguerolazaro@gmail.com>
2024-07-06 18:46:26 +0200

    limpieza de columnas hecha

--- src/app.py
+++ src/app.py
@@ -1,4 +1,11 @@
-from utils import db_connect
-engine = db_connect()
+import numpy as np
+import pandas as pd
 
-# your code here
+file = 'https://raw.githubusercontent.com/4GeeksAcademy/data-preprocessing-project-tutorial/main/AB_NYC_2019.csv'
+df_raw = pd.read_csv(file)
+
+
+df = df_raw.drop(['name', 'host_name', 'last_review', 'reviews_per_month', 'id', 'host_id'], axis=1)
+print(df.head)
+print("too many zeros in 'availability_365' observed, and no correlation with price seen. Suggesting to drop")
+print("same with 'number_of_reviews'")
